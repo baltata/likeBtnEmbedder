e58b430ca0f3b93885e1278791d364532c1d213e
baltata <128985806+baltata@users.noreply.github.com>
2023-03-27 08:33:22 +0200

    Update embedLikeBtn.js

--- embedLikeBtn.js
+++ embedLikeBtn.js
@@ -1,17 +1,32 @@
   
  
   
-  var correctionContainer=document.querySelector('.generalfeedback');
+var iframeList=document.querySelectorAll(".filter_embedquestion-iframe");
+var htmlElement='<span class="likebtn-wrapper" data-identifier="item_1"></span>';
    
-    function createID () {
+
+
+for (let iframe of iframeList) {
+
+
+  
+   
+  
+ 
+   
+  /*  function createID () {
         return window.location.href.split('catid=').pop().split('&')[0]+window.location.href.split('qid=').pop().split('&')[0]
     };
-    var id=createID();
-    var htmlElement=`<span class="likebtn-wrapper" data-identifier="${id}">`;
-    
-
-
-
+    /*var id=createID();*/
+    if (iframe.contentWindow.document.querySelector('.generalfeedback') !== null) {
+    var correctionContainer = iframe.contentWindow.document.querySelector('.generalfeedback');
+   
     correctionContainer.insertAdjacentHTML('beforeend',htmlElement);
     (function(d,e,s){if(d.getElementById("likebtn_wjs"))return;a=d.createElement(e);m=d.getElementsByTagName(e)[0];a.async=1;a.id="likebtn_wjs";a.src=s;m.parentNode.insertBefore(a, m)})(document,"script","//w.likebtn.com/js/w/widget.js");
-  
+   
+    
+}}
+(function(d,e,s){if(d.getElementById("likebtn_wjs"))return;a=d.createElement(e);m=d.getElementsByTagName(e)[0];a.async=1;a.id="likebtn_wjs";a.src=s;m.parentNode.insertBefore(a, m)})(document,"script","//w.likebtn.com/js/w/widget.js");
+    
+   
+    
